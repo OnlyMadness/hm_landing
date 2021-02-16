06519fe2a857e684c6b5b446c1b211edc1b7e8ae
OnlyMadness <rector96@mail.ru>
2021-02-16 19:07:02 +1000

    second commit

---
+++ app/js/app.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){ 
+	$('#container').masonry({
+// указываем элемент-контейнер в котором расположены блоки для динамической верстки
+	  itemSelector: '.justification__item',
+// указываем класс элемента являющегося блоком в нашей сетке
+		singleMode: true,
+// true - если у вас все блоки одинаковой ширины
+	  isResizable: false,
+// перестраивает блоки при изменении размеров окна
+	  isAnimated: false
+	}); 
+});
+
+$(document).ready(function(){ 
+	var $grid = $('#container').masonry({
+		itemSelector: '.justification__item',
+		percentPosition: true,
+		columnWidth: '.justification__item'
+	});
+	$grid.imagesLoaded().progress( function() {
+		$grid.masonry();
+	});  
+
+});
