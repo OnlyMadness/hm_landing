842894e3b32b57a5725bd37aba0f024aa9f92af8
OnlyMadness <rector96@mail.ru>
2021-03-01 19:09:13 +1000

    image compress, add callback modal, add burger menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,97 +1,151 @@
 /* Mansory */
-$(document).ready(function(){ 
+$(document).ready(function () {
 	$('#container').masonry({
-// указываем элемент-контейнер в котором расположены блоки для динамической верстки
-	  itemSelector: '.justification__item',
-// указываем класс элемента являющегося блоком в нашей сетке
+		// указываем элемент-контейнер в котором расположены блоки для динамической верстки
+		itemSelector: '.justification__item',
+		// указываем класс элемента являющегося блоком в нашей сетке
 		singleMode: true,
-// true - если у вас все блоки одинаковой ширины
-	  isResizable: false,
-// перестраивает блоки при изменении размеров окна
-	  isAnimated: false,
+		// true - если у вас все блоки одинаковой ширины
+		isResizable: false,
+		// перестраивает блоки при изменении размеров окна
+		isAnimated: false,
 		gutter: '.justification__item-space',
-	}); 
+	});
 });
 
 /* InputMask */
-$(document).ready(function(){
-		$('.consultation__phone').inputmask({"mask": "+7 (999) 999-9999"}); 
-    $('.consultation__email').inputmask("email");   
-    $('.questions__phone').inputmask({"mask": "+7 (999) 999-9999"}); 
-    $('.questions__email').inputmask("email");     
+$(document).ready(function () {
+	$('.consultation__phone').inputmask({
+		"mask": "+7 (999) 999-9999"
+	});
+	$('.consultation__email').inputmask("email");
+	$('.callback__phone').inputmask({
+		"mask": "+7 (999) 999-9999"
+	});
+	$('.callback__email').inputmask("email");
+	$('.questions__phone').inputmask({
+		"mask": "+7 (999) 999-9999"
+	});
+	$('.questions__email').inputmask("email");
 });
 
 /*Slick slider*/
 
-
-
-
 $('.cases__slider').slick({
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
+	infinite: false,
+	slidesToShow: 2,
+	slidesToScroll: 1,
 	responsive: [
-    {
-      breakpoint: 782,
-      settings: {
+		{
+			breakpoint: 782,
+			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
 				dots: true
-      }
+			}
     }
 	],
-	prevArrow:"<button type='button' class='slick-prev'></button>",
-	nextArrow:"<button type='button' class='slick-next'></button>",
+	prevArrow: "<button type='button' class='slick-prev'></button>",
+	nextArrow: "<button type='button' class='slick-next'></button>",
 });
 
 /*Modal*/
 
+$(function () {
+	$("#fresh").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-fresh').arcticmodal();
+	});
 
-$(function(){
-    $('#modal-snab-vostok').arcticmodal();
+	$("#item-fresh").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-fresh').arcticmodal();
+	});
+
+	$("#tigre").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-tigre').arcticmodal();
+	});
+
+	$("#item-tigre").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-tigre').arcticmodal();
+	});
+
+	$("#bakkon").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-bakkon').arcticmodal();
+	});
+
+	$("#item-bakkon").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-bakkon').arcticmodal();
+	});
+
+	$("#marathon").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-marathon').arcticmodal();
+	});
+
+	$("#item-marathon").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-marathon').arcticmodal();
+	});
+
+	$("#snab").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-snab').arcticmodal();
+	});
+
+	$("#item-snab").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-snab').arcticmodal();
+	});
+
+
+
+	$(".btn--black").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-callback').arcticmodal();
+	});
+
+	$(".btn--violet--underline").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-experience').arcticmodal();
+	});
+
+	$(".btn--audit").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-audit').arcticmodal();
+	});
+
+	$(".btn--marketing").on("click", function (event) {
+		event.preventDefault();
+		$('#modal-marketing').arcticmodal();
+	});
+
+
 });
 
-/* modal-snab-vostok.onclick = function(event) {
-	
-};*/
+/*Smooth scroll*/
 
 $(function () {
-    /*Smooth scroll*/
-    $("[data-scroll]").on("click", function(event){
-        event.preventDefault(); /*отменяет перезагрузку страницы по сылке*/
-        $('#nav').removeClass('show');
-        $('.burger__item').removeClass('active');
-        let  elementId = $(this).data('scroll'); /*получаем id на который кликнули*/
-        let  elementOffset = $(elementId).offset().top; /*получаем высоту от верха до начала элемента*/
-        $("html, body").animate({
-            scrollTop : elementOffset 
-        }, 700);
-    });
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault(); /*отменяет перезагрузку страницы по сылке*/
+		$('.nav').removeClass('active');
+		$('.btn--black').removeClass('active');
+		$('.ham7').removeClass('active');
+		let elementId = $(this).data('scroll'); /*получаем id на который кликнули*/
+		let elementOffset = $(elementId).offset().top; /*получаем высоту от верха до начала элемента*/
+		$("html, body").animate({
+			scrollTop: elementOffset
+		}, 700);
+	});
 });
 
-$(function(){
-   $('.btn--burger').on("click", function(event){
-       $('#nav').toggleClass('show'); 
-       $('.burger__item').toggleClass('active');
-   });
+$(function () {
+	$('.burger').on("click", function (event) {
+		$('.nav').toggleClass('active');
+		$('.btn--black').toggleClass('active');
+	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
